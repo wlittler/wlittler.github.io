a0f7c92e8aece4c2102627f5bf48fba9c06e5874
Will Littler <williamm.littler@gmail.com>
2016-03-28 21:48:14 -0500

    Parallax / Boostrap
    Added parallax effect to bootstrap homework project. Spent too much time
    playing with fonts... and lots of other things.

--- AustinCodingAcademy_Spring2016/Week7/bootstrap_project/js/parallax.js
+++ AustinCodingAcademy_Spring2016/Week7/bootstrap_project/js/parallax.js
@@ -1,52 +1,16 @@
-/**
- * Author: Heather Corey
- * jQuery Simple Parallax Plugin
- *
- */
- 
-(function($) {
- 
-    $.fn.parallax = function(options) {
- 
-        var windowHeight = $(window).height();
- 
-        // Establish default settings
-        var settings = $.extend({
-            speed        : 0.15
-        }, options);
- 
-        // Iterate over each object in collection
-        return this.each( function() {
- 
-        	// Save a reference to the element
-        	var $this = $(this);
- 
-        	// Set up Scroll Handler
-        	$(document).scroll(function(){
- 
-    		        var scrollTop = $(window).scrollTop();
-            	        var offset = $this.offset().top;
-            	        var height = $this.outerHeight();
- 
-    		// Check if above or below viewport
-			if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-				return;
-			}
- 
-			var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
- 
-                 // Apply the Y Background Position to Set the Parallax Effect
-    			$this.css('background-position', 'center ' + yBgPosition + 'px');
-                
-        	});
-        });
-    }
-}(jQuery));
-
-$('.bg-1,.bg-3').parallax({
-	speed :	0.15
-});
-
-$('.bg-2').parallax({
-	speed :	0.25
-});
+$(document).ready(function(){       
+                var scroll_start = 0;
+                var startchange = $('#startchange');
+                var offset = startchange.offset();
+                    if (startchange.length){
+                $(document).scroll(function() { 
+                    scroll_start = $(this).scrollTop();
+                    if(scroll_start > offset.top) {
+//                          $(".navbar-default").css("background-image", "url('../img/cheetos_background.jpg')");
+                        $(".navbar-default").css('background-color', '#E4AA33');
+                       } else {
+                          $('.navbar-default').css('background-color', 'transparent');
+                       }
+                   });
+                    }
+                });

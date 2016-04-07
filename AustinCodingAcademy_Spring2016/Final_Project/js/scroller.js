647e30ee7b8f51ef891eb2f002ce292c1b0f4a11
Will Littler <williamm.littler@gmail.com>
2016-04-06 21:17:46 -0500

    final project updates
    to how, why, index, js, css

--- AustinCodingAcademy_Spring2016/Final_Project/js/scroller.js
+++ AustinCodingAcademy_Spring2016/Final_Project/js/scroller.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     var scroll_start = 0;
     var startchange = $('#startchange');
     var offset = startchange.offset();
+    var fadeNav = 'animated fadeInDown navbar-fixed-top';
+    //    var navFixed = 'navbar-fixed-top';
+    var animatedEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
     if (startchange.length) {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $("nav").addClass("navbar-fixed-top");
+                $("nav").addClass(fadeNav);
                 $("section.forest_bg").addClass("extend_forest");
             } else {
                 $("nav").removeClass("navbar-fixed-top");
@@ -14,5 +18,45 @@ $(document).ready(function () {
             }
             //            console.log(scroll_start);
         });
+
     }
+
+
+
+
+
+
 });
+
+
+//Unmolested code
+
+//if (startchange.length) {
+//        $(document).scroll(function () {
+//            scroll_start = $(this).scrollTop();
+//            if (scroll_start > offset.top) {
+//                $("nav").addClass("navbar-fixed-top animated fadeInDown");
+//                $("section.forest_bg").addClass("extend_forest");
+//            } else {
+//                $("nav").removeClass("navbar-fixed-top");
+//                $("section.forest_bg").removeClass("extend_forest");
+//            }
+//            //            console.log(scroll_start);
+//        });
+
+//2nd try
+
+//  if (startchange.length) {
+//        $(document).scroll(function () {
+//            scroll_start = $(this).scrollTop();
+//            if (scroll_start > offset.top) {
+//                $("nav").addClass(fadeNav).one(animatedEnd, function () {
+//                    $(this).removeClass(fadeNav);
+//                });
+//                $("section.forest_bg").addClass("extend_forest");
+//            } else {
+//                $("nav").removeClass("navbar-fixed-top");
+//                $("section.forest_bg").removeClass("extend_forest");
+//            }
+//            //            console.log(scroll_start);
+//        });
